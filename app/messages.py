8cdcf65b0f8a9a86cdead17f589ce294b4f82b16
Salman Ami Motlaq <66294582+SMotlaq@users.noreply.github.com>
2022-12-23 18:04:17 +0330

    Update messages.py

--- app/messages.py
+++ app/messages.py
@@ -2,8 +2,8 @@ start = 'ping OK'
 not_athorized = 'شما در لیست افراد مجاز برای استفاده از این بات نیستید'
 fozool_detected = '👀 Fozool detected 👀'
 no_arg = 'Arg error'
-traffic = '''TX 🔺 & GiB
-RX 🔻 % GiB
+traffic = '''TX 🔺 & 
+RX 🔻 % 
 Ratio 📊 $'''
 done = 'Done uploading. Press /get_stat'
 upload_error = 'An exception occcured during upload'
